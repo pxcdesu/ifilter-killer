91b5d5217d38d822e69304cb96ebf47b188fdd46
Villager A <100194237+habataku@users.noreply.github.com>
2023-03-22 22:57:56 +0900

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://easytutoring.app/bare/',
+    bare: '/bare/',
     encodeUrl: Ultraviolet.codec.plain.encode,
     decodeUrl: Ultraviolet.codec.plain.decode,
     handler: '/uv/uv.handler.js',
