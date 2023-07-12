f92b26bc2fab0b55d8ce856daec6e8add1f9707d
Villager A <100194237+habataku@users.noreply.github.com>
2023-07-12 20:38:40 +0900

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: '/bare/',
+    bare: 'https://naughty-tiara-bee.cyclic.app/bare/',
     encodeUrl: Ultraviolet.codec.plain.encode,
     decodeUrl: Ultraviolet.codec.plain.decode,
     handler: '/uv/uv.handler.js',
