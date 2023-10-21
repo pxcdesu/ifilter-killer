ef837f347d51076d4788325a9051bc4654eb8b25
Villager A <100194237+habataku@users.noreply.github.com>
2023-10-21 11:55:39 +0900

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://onlineschool.design/bare/',
+    bare: 'https://uv.linearfunctions.net/',
     encodeUrl: Ultraviolet.codec.plain.encode,
     decodeUrl: Ultraviolet.codec.plain.decode,
     handler: '/uv/uv.handler.js',
