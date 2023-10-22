e845b43f25ca7650ebac1a93387e733096384adf
Villager A <100194237+habataku@users.noreply.github.com>
2023-10-22 14:10:29 +0900

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://uv.linearfunctions.net/',
+    bare: 'https://worrisome-tan-gopher.cyclic.app/bare/',
     encodeUrl: Ultraviolet.codec.plain.encode,
     decodeUrl: Ultraviolet.codec.plain.decode,
     handler: '/uv/uv.handler.js',
