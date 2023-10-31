2f7625d0fa9d7351017a43851bcc9673310576d4
Villager A <100194237+habataku@users.noreply.github.com>
2023-10-31 12:29:21 +0900

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://acutemath.quest/bare/',
+    bare: 'https://azsxdcfvgbhnjmk.f5.si/bare/',
     encodeUrl: Ultraviolet.codec.plain.encode,
     decodeUrl: Ultraviolet.codec.plain.decode,
     handler: '/uv/uv.handler.js',
