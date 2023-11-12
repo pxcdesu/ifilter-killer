045c84c1b51a591afc0c014a61cb239e50cd564b
Villager A <100194237+habataku@users.noreply.github.com>
2023-11-12 23:59:45 +0900

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: ['https://polarislearning.org/bare/','https://bare-server-vercel.vercel.app/bare/'],
+    bare: ['https://bare-server-vercel.vercel.app/bare/'],
     encodeUrl: Ultraviolet.codec.plain.encode,
     decodeUrl: Ultraviolet.codec.plain.decode,
     handler: '/uv/uv.handler.js',
