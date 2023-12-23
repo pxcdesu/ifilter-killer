a3b0a452ba8db95a5bff5f3b3d73d67669b2bc5a
Villager A <100194237+habataku@users.noreply.github.com>
2023-12-23 17:53:49 +0900

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: ['https://illilililililililiililiiiililililililililiiilii-3rudscki.b4a.run/'],
+    bare: ['https://holy-v2-skyegamesyt.koyeb.app/bare/'],
     encodeUrl: Ultraviolet.codec.plain.encode,
     decodeUrl: Ultraviolet.codec.plain.decode,
     handler: '/uv/uv.handler.js',
