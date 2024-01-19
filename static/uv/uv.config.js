b950df0b2e3494f2aef3079e93ff562ed7976687
Villager A <100194237+habataku@users.noreply.github.com>
2024-01-19 16:37:20 +0900

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/t/',
-    bare: ['https://kaguya-f9xxqqfr.b4a.run/bare/'],
+    bare: ['https://savethetrees.sen.org.nz/bare/'],
     encodeUrl: Ultraviolet.codec.plain.encode,
     decodeUrl: Ultraviolet.codec.plain.decode,
     handler: '/uv/uv.handler.js',
