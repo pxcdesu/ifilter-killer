f35880499b9e081e6f00a4a94544e07312ef4196
Villager A <100194237+habataku@users.noreply.github.com>
2024-02-01 21:09:40 +0900

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/t/',
-    bare: ['https://illilililililililiililiiiililililililililiiilii-3rudscki.b4a.run/'],
+    bare: ['/bare/'],
     encodeUrl: Ultraviolet.codec.plain.encode,
     decodeUrl: Ultraviolet.codec.plain.decode,
     handler: '/uv/uv.handler.js',
